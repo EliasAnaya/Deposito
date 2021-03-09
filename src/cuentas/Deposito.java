2f1997db2b2f26036a2900f204a96d1bfb3dd2e2
eland <eliasanayadiazcordoves@gmail.com>
2021-03-09 13:13:46 +0100

    Segunda subida, metodo void y cambiamos lo que se le añade al salario por 1500

--- src/cuentas/Deposito.java
+++ src/cuentas/Deposito.java
@@ -18,18 +18,18 @@ public class Deposito {
         cantidad();
     }
 
-    private static float cantidad() {
+    private static void cantidad() {
         CCuenta cuenta1;
         double saldoActual;
         
-        cuenta1 = new CCuenta("Antonio Lopez","1000-2365-85-123456789",2500,0);
+        cuenta1 = new CCuenta("Antonio Lopez","1000-2365-85-123456789",1500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+saldoActual );
         
         try{
             cuenta1.retirar(2300);
         }catch (Exception e) {
-            System.out.println("Fallo al retirar");
+            System.out.println("Fallo tecnico");
         }
         try{
             System.out.println("Ingreso en cuenta");
